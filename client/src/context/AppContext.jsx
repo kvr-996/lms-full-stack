32c55ca09c4d4000a1af5ddad92cd52df869dafe
kvr-996 <keertivarma.vr@gmail.com>
2025-08-05 02:17:06 +0530

    till starting

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,28 +5,25 @@ import { toast } from "react-toastify";
 import { useAuth, useUser } from "@clerk/clerk-react";
 import humanizeDuration from "humanize-duration";
 import { dummyCourses } from "../assets/assets";
-
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const currency = import.meta.env.VITE_CURRENCY;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const currency = import.meta.env.VITE_CURRENCY
+  const navigate = useNavigate();
+  const { getToken } = useAuth();
+  const { user } = useUser();
 
-    const navigate = useNavigate()
-    const { getToken } = useAuth()
-    const { user } = useUser()
+  const [showLogin, setShowLogin] = useState(false);
+  const [isEducator, setIsEducator] = useState(true);
+  const [allCourses, setAllCourses] = useState([]);
+  const [userData, setUserData] = useState(null);
+  const [enrolledCourses, setEnrolledCourses] = useState([]);
 
-    const [showLogin, setShowLogin] = useState(false)
-    const [isEducator,setIsEducator] = useState(true)
-    const [allCourses, setAllCourses] = useState([] )
-    const [userData, setUserData] = useState(null)
-    const [enrolledCourses, setEnrolledCourses] = useState([])
-
-    // Fetch All Courses
-    const fetchAllCourses = async () => {
-
-       /* try {
+  // Fetch All Courses
+  const fetchAllCourses = async () => {
+    /* try {
 
             const { data } = await axios.get(backendUrl + '/api/course/all');
 
@@ -39,48 +36,48 @@ export const AppContextProvider = (props) => {
         } catch (error) {
             toast.error(error.message)
         }*/
-       setAllCourses(dummyCourses)
+    setAllCourses(dummyCourses);
+  };
 
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+  // Fetch UserData
+  const fetchUserData = async () => {
+    try {
+      if (user.publicMetadata.role === "educator") {
+        setIsEducator(true);
+      }
+
+      const token = await getToken();
+
+      const { data } = await axios.get(backendUrl + "/api/user/data", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (data.success) {
+        setUserData(data.user);
+      } else toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
     }
-    
-    useEffect(() => {
-        fetchAllCourses()
-    }, [])
-    // Fetch UserData 
-    const fetchUserData = async () => {
-
-        try {
-
-            if (user.publicMetadata.role === 'educator') {
-                setIsEducator(true)
-            }
-
-            const token = await getToken();
-
-            const { data } = await axios.get(backendUrl + '/api/user/data',
-                { headers: { Authorization: `Bearer ${token}` } })
-
-            if (data.success) {
-                setUserData(data.user)
-            } else (
-                toast.error(data.message)
-            )
-
-        } catch (error) {
-            toast.error(error.message)
-        }
-
+  };
+  const fetchUserEnrolledCourses = async () => {
+    setEnrolledCourses(dummyCourses);
+  };
+  useEffect(() => {
+    fetchAllCourses();
+    fetchUserEnrolledCourses();
+  }, []);
+  const logToken = async () => {
+    console.log(await getToken());
+  };
+  useEffect(() => {
+    if (user) {
+      logToken();
     }
-    const fetchUserEnrolledCourses=async ()=>
-    {
-        setEnrolledCourses(dummyCourses);
-    }
-    useEffect(()=>
-    {
-        fetchAllCourses()
-        fetchUserEnrolledCourses()
-    },[])
-/*//ACTUAL CODE...
+  }, [user]);
+  /*//ACTUAL CODE...
     // Fetch User Enrolled Courses
     const fetchUserEnrolledCourses = async () => {
 
@@ -105,73 +102,72 @@ export const AppContextProvider = (props) => {
         }
     }, [user])*/
 
-    // Function to Calculate Course Chapter Time
-    const calculateChapterTime = (chapter) => {
+  // Function to Calculate Course Chapter Time
+  const calculateChapterTime = (chapter) => {
+    let time = 0;
 
-        let time = 0
+    chapter.chapterContent.map((lecture) => (time += lecture.lectureDuration));
 
-        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+    return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+  };
 
-        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] })
+  // Function to Calculate Course Duration
+  const calculateCourseDuration = (course) => {
+    let time = 0;
 
+    course.courseContent.map((chapter) =>
+      chapter.chapterContent.map((lecture) => (time += lecture.lectureDuration))
+    );
+
+    return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+  };
+
+  //Function to calculate average rating of course
+  const calculateRating = (course) => {
+    if (course.courseRatings.length === 0) {
+      return 0;
     }
 
-    // Function to Calculate Course Duration
-    const calculateCourseDuration = (course) => {
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating;
+    });
+    return Math.floor(totalRating / course.courseRatings.length);
+  };
 
-        let time = 0
+  //Function to calculate the number of lectures in the course
+  const calculateNoOfLectures = (course) => {
+    let totalLectures = 0;
+    course.courseContent.forEach((chapter) => {
+      if (Array.isArray(chapter.chapterContent)) {
+        totalLectures += chapter.chapterContent.length;
+      }
+    });
+    return totalLectures;
+  };
 
-        course.courseContent.map(
-            (chapter) => chapter.chapterContent.map(
-                (lecture) => time += lecture.lectureDuration
-            )
-        )
+  const value = {
+    showLogin,
+    setShowLogin,
+    backendUrl,
+    currency,
+    navigate,
+    userData,
+    setUserData,
+    getToken,
+    allCourses,
+    fetchAllCourses,
+    enrolledCourses,
+    fetchUserEnrolledCourses,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateRating,
+    calculateNoOfLectures,
+    isEducator,
+    setIsEducator,
+  };
 
-        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] })
-
-    }
-
-    //Function to calculate average rating of course
-    const calculateRating = (course) => {
-
-        if (course.courseRatings.length === 0) {
-            return 0
-        }
-
-        let totalRating = 0
-        course.courseRatings.forEach(rating => {
-            totalRating += rating.rating
-        })
-        return Math.floor(totalRating / course.courseRatings.length)
-    }
-
-    //Function to calculate the number of lectures in the course
-    const calculateNoOfLectures = (course) => {
-        let totalLectures = 0;
-        course.courseContent.forEach(chapter => {
-            if (Array.isArray(chapter.chapterContent)) {
-                totalLectures += chapter.chapterContent.length;
-            }
-        });
-        return totalLectures;
-    }
-
-
-    const value = {
-        showLogin, setShowLogin,
-        backendUrl, currency, navigate,
-        userData, setUserData, getToken,
-        allCourses, fetchAllCourses,
-        enrolledCourses, fetchUserEnrolledCourses,
-        calculateChapterTime, calculateCourseDuration,
-        calculateRating, calculateNoOfLectures,
-        isEducator,setIsEducator
-    }
-
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-
-}
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
--- server/server.js
+++ server/server.js
@@ -3,13 +3,17 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebHooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 await connectDB();
 app.use(cors());
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => res.send("API WORKING"));
 app.post("/clerk", express.json(), clerkWebHooks);
+app.use("/api/educator", express.json(), educatorRouter);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
