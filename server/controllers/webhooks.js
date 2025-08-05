db85d873206976acf9301bac6ed19f151dc885a7
kvr-996 <keertivarma.vr@gmail.com>
2025-08-05 13:54:55 +0530

    added new_files

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,5 +1,8 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
+import Stripe from "stripe";
+import Purchase from "../models/Purchase.js";
+import Course from "../models/Course.js";
 
 export const clerkWebHooks = async (req, res) => {
   try {
@@ -24,7 +27,7 @@ export const clerkWebHooks = async (req, res) => {
       }
       case "user.updated": {
         const userData = {
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           imageUrl: data.imageUrl,
         };
@@ -42,5 +45,59 @@ export const clerkWebHooks = async (req, res) => {
     }
   } catch (error) {
     res.json({ success: true, message: error.message });
+  }
+};
+
+const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
+export const stripewebhooks = async (req, res) => {
+  const sig = req.headers["stripe-signature"];
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    res.status(400).send(`Webhook Eroor:${err.message}`);
+  }
+  switch (event.type) {
+    case "payment_intent.succeeded": {
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id;
+      const session = await stripeInstance.checkout.sessions.list({
+        payment_intent: paymentIntentId,
+      });
+      const { purchaseId } = session.data[0].metadata;
+      const purchaseData = await Purchase.findById(purchaseId);
+      const userData = await User.findById(purchaseData.userId);
+      const courseData = await Course.findById(
+        purchaseData.courseId.toString()
+      );
+      courseData.enrolledStudents.push(userData);
+      await courseData.save();
+
+      userData.enrolledCourses.push(courseData._id);
+      await userData.save();
+      purchaseData.status = "completed";
+      await purchaseData.save();
+      console.log("Payment INtnet was successful");
+      break;
+    }
+
+    case "payment_method.payment_failed":
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id;
+      const session = await stripeInstance.checkout.sessions.list({
+        payment_intent: paymentIntentId,
+      });
+      const { purchaseId } = session.data[0].metadata;
+      const purchaseData = await Purchase.findById(purchaseId);
+      purchaseData.status = "failed";
+      await purchaseData.save();
+      console.log("Payment was attached to a customer");
+      break;
+    default:
+      console.log(`Unhandled event type:${event.type}`);
   }
 };
--- server/server.js
+++ server/server.js
@@ -2,18 +2,25 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
-import { clerkWebHooks } from "./controllers/webhooks.js";
+import { clerkWebHooks, stripewebhooks } from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
 import { clerkMiddleware } from "@clerk/express";
+import connectCloudinary from "./configs/cloudinary.js";
+import courseRouter from "./routes/courseRoute.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 await connectDB();
+await connectCloudinary();
 app.use(cors());
 app.use(clerkMiddleware());
-
 app.get("/", (req, res) => res.send("API WORKING"));
 app.post("/clerk", express.json(), clerkWebHooks);
 app.use("/api/educator", express.json(), educatorRouter);
+app.use("/api/course", express.json(), courseRouter);
+app.use("/api/user", express.json(), userRouter);
+app.use("/stripe", express.raw({ type: "application/json" }), stripewebhooks);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
